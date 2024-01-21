6070a448b8172ba732680ae3a726c8482a614ad5
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-22 08:53:32 +1300

    Update encoding options in config file

--- src/config/encoding.ts
+++ src/config/encoding.ts
@@ -1,5 +1,14 @@
 const ENCODING = {
-    matchesCSV: 'utf-8'
+  UTF8: 'utf-8',
+  UTF16: 'utf-16',
+  UTF32: 'utf-32',
+  ASCII: 'ascii',
+  BASE64: 'base64',
+  LATIN1: 'latin1',
+  HEX: 'hex',
+  BINARY: 'binary',
+  UCS2: 'ucs2',
+  WIN1252: 'win1252',
 }
 
 export default ENCODING
