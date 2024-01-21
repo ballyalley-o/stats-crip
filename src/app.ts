1663a4df6b13cf456f0ba9764758dc0e305b7ca1
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-22 02:16:51 +1300

    Refactor app.ts to use MatchReader class for reading CSV data

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,19 @@
 import fs from 'fs'
-import { PATH, VARS } from '@constants'
+import { PATH } from '@constants'
+import { VARS } from '@enums'
 import { ENCODING } from '@config'
+// @classes
+import { MatchReader } from '@csv'
 
-const games = fs
-  .readFileSync(PATH.STATS_CSV, {
-    encoding: ENCODING.matchesCSV as BufferEncoding,
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+const reader = new MatchReader(PATH.STATS_CSV)
+reader.read()
 
 let manchesterWins = 0
 let team = 'Man United'
 let home = VARS.home
 let away = VARS.away
 
-for (let game of games) {
+for (let game of reader.data) {
   if (game[1] === team && game[5] === home) {
     manchesterWins++
   } else if (game[2] === team && game[5] === away) {
