1f7191f7b138326af40cfc7fde1b9e0a2f1b061b
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-22 02:17:48 +1300

    Add MatchReader class to read CSV files

---
+++ src/csv/match-reader.ts
@@ -0,0 +1,20 @@
+import { CSVFileReader } from '@csv'
+import { VARS } from '@enums'
+import { dateParse } from '@utils'
+import { MatchData } from '@types'
+
+class MatchReader extends CSVFileReader<MatchData> {
+  mapRow(row: string[]): MatchData {
+    return [
+      dateParse(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      row[5] as VARS,
+      row[6],
+    ]
+  }
+}
+
+export default MatchReader
