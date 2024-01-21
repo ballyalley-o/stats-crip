596fa9b7ff2f26a3bdc5b04b162852d83f93c75a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-22 02:56:17 +1300

    🦄 refactor(Add CSVFileReader and MatchReader classes abstract refactor):

---
+++ src/csv/inherit/csv-file-reader.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import { ENCODING } from '@config'
+import { VARS } from '@enums'
+
+type MatchData = [Date, string, string, number, number, VARS, string]
+
+abstract class CSVFileReader<T> {
+  data: T[] = []
+
+  constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: ENCODING.matchesCSV as BufferEncoding,
+      })
+      .split('\n')
+      .map((row: string): string[] => {
+        return row.split(',')
+      })
+      .map(this.mapRow)
+  }
+}
+
+export default CSVFileReader
