fe17e1cac4225cf700d87aa62a5032ea0f3368a2
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-22 02:16:27 +1300

    Add build and start scripts to cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo  "${BLUEBG} ⚡️ Initializing build .. . ${RESET} "
+
+npm run build
+
+
+echo  "${BLUEBG} 🔋 DONE ${RESET} "
+
+echo  "${BLUEBG} 🚀 Starting server .. . ۞ ${RESET} "
+
+npm start
