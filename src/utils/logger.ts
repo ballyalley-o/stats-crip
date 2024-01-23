0aeb5a9efdcfa1e5fbfcffa4191299411115677d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-23 15:32:34 +1300

    Add ILogger interface to interfaces

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,28 +1,40 @@
-import colors from 'colors'
+import 'colors'
+import { ILogger } from '@interfaces'
 
-class Logger {
-  constructor() {
-    this.error = this.error.bind(this)
-    this.warn = this.warn.bind(this)
-    this.info = this.info.bind(this)
-    this.log = this.log.bind(this)
-  }
-
-  log(...args: string[]) {
-    this.log(colors.green(...(args as [string])))
-  }
-
-  info(...args: string[]) {
-    this.info(colors.blue(...(args as [string])))
-  }
-
-  warn(...args: string[]) {
-    this.warn(colors.yellow(...(args as [string])))
-  }
-
-  error(...args: string[]) {
-    this.error(colors.red(...(args as [string])))
+declare module 'colors' {
+  interface String {
+    yellow: string
+    bgCyan: string
+    bgRed: string
+    red: string
+    bgYellow: string
+    // errored: any[]
   }
 }
 
-export default Logger
+const logger: ILogger = {
+  // @type - custom
+  custom: (color: any, ...message: string[]) =>
+    console.log(message.join('')[color]),
+
+  // @type - log :Default
+  log: (...message: string[]) => console.log(message.join(' ').yellow),
+
+  // @type - info
+  info: (...message: string[]) => console.log(message.join(' ').bgCyan),
+
+  // @type - warn
+  warn: (...message: string[]) => console.warn(message.join(' ').bgYellow),
+
+  // @type - table -for array and obj
+  tbl: (...message: any[]) => console.table(message),
+
+  // @type - error
+  error: (...message: string[]) =>
+    console.log(new Error(message.join(' ').bgRed)),
+
+  // @type - debug
+  debug: (...message: string[]) => console.debug(message.join(' ').bgRed),
+}
+
+export default logger
