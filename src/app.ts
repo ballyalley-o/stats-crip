c27b49fa1661124e8bc4c40d3d3eecd9a04b4b83
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-23 16:24:22 +1300

    🦄 refactor(Refactor app.ts to use MatchReader.fromCSV and GameSummary.winsAnalysisHTMLReport):

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,12 @@
-import fs from 'fs'
 import { PATH } from '@constants'
-import { VARS } from '@enums'
 // @classes
-import { MatchReader, CSVFileReader } from '@csv'
-import { ConsoleReport } from '@report-targets'
-import { WinsAnalysis, GameSummary } from '@classes'
-
-const csvFileReader = new CSVFileReader(PATH.STATS_CSV)
-
-const matchReader = new MatchReader(csvFileReader)
-matchReader.load()
+import { MatchReader } from '@csv'
+import { GameSummary } from '@classes'
 
 let team = 'Man United'
 
-// let manchesterWins = 0
-// let home = VARS.home
-// let away = VARS.away
+const matchReader = MatchReader.fromCSV(PATH.STATS_CSV)
+const summary = GameSummary.winsAnalysisHTMLReport(team)
 
-// for (let game of matchReader.matches) {
-//   if (game[1] === team && game[5] === home) {
-//     manchesterWins++
-//   } else if (game[2] === team && game[5] === away) {
-//     manchesterWins++
-//   }
-// }
-
-const summary = new GameSummary(new WinsAnalysis(team), new ConsoleReport())
+matchReader.load()
 summary.buildPrintReport(matchReader.matches)
