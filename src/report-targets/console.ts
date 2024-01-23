00a1e275b85afb771f6e987efb6527bb05163209
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-23 16:24:06 +1300

    Add printInfo, printWarn, and printError methods to ConsoleReport

--- src/report-targets/console.ts
+++ src/report-targets/console.ts
@@ -5,6 +5,15 @@ class ConsoleReport implements IOutputTarget {
   print(report: string): void {
     logger.log(report)
   }
+  printInfo(report: string): void {
+    logger.info(report)
+  }
+  printWarn(report: string): void {
+    logger.info(report)
+  }
+  printError(report: string): void {
+    logger.error(report)
+  }
 }
 
 export default ConsoleReport
